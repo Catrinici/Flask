56355c98525d9bf4f78a4cb4a19d4f737bf31616
Catrinici <catrinici.antonina@gmail.com>
2019-08-28 11:22:18 -0700

    first commit

--- flask_MySQL/morepractice.sql
+++
@@ -1,3 +0,0 @@
-
-DELETE FROM blogs_erd.users WHERE (id = '6');
-select * from users
--- flask_mysql/server.py
+++ flask_mysql/server.py
@@ -5,10 +5,5 @@ app = Flask(__name__)
 mysql = connectToMySQL('mydb')
 
 print("all the users", mysql.query_db("SELECT * FROM users;"))
-
-
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)
